ecf54cbdf8a67ccfde420454cd6683537c62ddb7
carly <carly@noukod.com>
2018-09-24 14:50:05 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,20 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves name# code an argument here
   # Your code here
+  array.each_with_index { |index, name| puts " #{index + 1} #{name}"
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet elements# code an argument here
   # Your code here
+  elements.collect {|element| "#{element.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls calls# code an argument here
   # Your code here
+  calls.any? {|call| call.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese cheeses# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheeses.find {|cheese| cheese_types.include?(string)}
 end
